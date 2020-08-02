739f6f97b819311fe018cfa907b55b39efd12fc2
galy <galy@student.42.fr>
2020-08-02 11:47:49 +0200

    add flip move table gen

--- src/resolver/Two_Phase/moves.ts
+++ src/resolver/Two_Phase/moves.ts
@@ -9,7 +9,7 @@ import D from './defines';
  ********************************************/
 const FILE_DIR_TABLE = './tables'
 const FILE_MOVE_TWIST = "move_twist.rbk";
-export let twistMove: Array<Number>;
+export let twistMove: Array<number>;
 const cc = new CubieCube();
 if (!fs.existsSync(`${FILE_DIR_TABLE}/${FILE_MOVE_TWIST}`)) {
   console.log(`creating ${FILE_MOVE_TWIST} table...`);
@@ -38,11 +38,43 @@ if (!fs.existsSync(`${FILE_DIR_TABLE}/${FILE_MOVE_TWIST}`)) {
   const strFromFile = fs.readFileSync(`${FILE_DIR_TABLE}/${FILE_MOVE_TWIST}`, 'utf8');
   const arrStr = Array.from(strFromFile);
   arrStr.map((val, idx) => {
-    let number = new Number(val);
+    let number = Number(val);
     if (Number.isNaN(number)) {
       throw(new Error('[ERROR] moves: Twist Move from file error'))
     } else {
       twistMove[idx] = number;
     }
   })
-}
+};
+
+
+/****************************************
+ * Move table for the flip of the edges *
+ ****************************************/
+
+const FILE_MOVE_FLIP = "move_flip.rbk";
+let flipMove: Array<number>;
+let basicMove: CubieCube | undefined;
+if (!fs.existsSync(`${FILE_DIR_TABLE}/${FILE_MOVE_FLIP}`)) {
+  console.log(`Creating ${FILE_MOVE_FLIP} table...`);
+  flipMove = _.fill(new Array(D.N_FLIP * D.N_MOVE), 0);
+  for (let i = 0; i < D.N_FLIP; i++) {
+    cc.setFlip(i);
+    for (let j = 0; j < D.NB_COLORS; j++) {
+      for (let k = 0; k < 3; k++) {
+        basicMove = CubieCube.basicMoveCube(j);
+        if (!basicMove) throw(new Error('[ERROR] move - basic move value: undefined.'));
+        cc.edgeMultiply(basicMove)
+        flipMove[D.N_MOVE * i + 3 * j + k] = cc.getFlip();
+      }
+      basicMove = CubieCube.basicMoveCube(j);
+      if (!basicMove) throw(new Error('[ERROR] move - basic move value: undefined.'));
+      cc.edgeMultiply(basicMove);
+    }
+  }
+  const str = JSON.stringify(flipMove);
+  fs.writeFileSync(`${FILE_DIR_TABLE}/${FILE_MOVE_FLIP}`, str);
+} else {
+  console.log(`Loading ${FILE_MOVE_FLIP} tables...`);
+  flipMove = JSON.parse(require(`${FILE_DIR_TABLE}/${FILE_MOVE_FLIP}`));
+};
